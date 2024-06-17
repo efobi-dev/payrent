6ae7b34bbd97ca51512ccafd15acb62c95d10c03
Owen <jenukohaefobi@gmail.com>
2024-06-17 00:16:25 -0700

    fix: coming soon on Paackages page,update page.tsx

---
+++ app/packages/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import ComingSoon from "@/components/CominSoon";
+
+export default function Page() {
+  return (
+    <section>
+      <ComingSoon />
+    </section>
+  );
+}
